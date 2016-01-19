6d70602ff301d93b29ce5b1ebcf1a78c7e0ed52f
Marton Imre Farkas <marton_imre_farkas@epam.com>
2016-01-19 15:08:33 +0100

    our of Heroes - 4. User Input

--- TourOfHeroes/angular2-tour-of-heroes/app/app.component.js
+++ TourOfHeroes/angular2-tour-of-heroes/app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './hero'], function(exports_1) {
+System.register(['angular2/core', './click-me.component', './click-me2.component', './loop-back.component', './keyup.components', './little-tour.component'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,32 +8,42 @@ System.register(['angular2/core', './hero'], function(exports_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, hero_1;
+    var core_1, click_me_component_1, click_me2_component_1, loop_back_component_1, keyup_components_1, little_tour_component_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
-            function (hero_1_1) {
-                hero_1 = hero_1_1;
+            function (click_me_component_1_1) {
+                click_me_component_1 = click_me_component_1_1;
+            },
+            function (click_me2_component_1_1) {
+                click_me2_component_1 = click_me2_component_1_1;
+            },
+            function (loop_back_component_1_1) {
+                loop_back_component_1 = loop_back_component_1_1;
+            },
+            function (keyup_components_1_1) {
+                keyup_components_1 = keyup_components_1_1;
+            },
+            function (little_tour_component_1_1) {
+                little_tour_component_1 = little_tour_component_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
-                    this.title = 'Tour of Heroes';
-                    this.heroes = [
-                        new hero_1.Hero(1, 'Windstorm'),
-                        new hero_1.Hero(13, 'Bombasto'),
-                        new hero_1.Hero(15, 'Magneta'),
-                        new hero_1.Hero(20, 'Tornado')
-                    ];
-                    this.myHero = this.heroes[0];
                 }
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n      <h1>{{title}}</h1>\n      <h2>My favorite hero is: {{myHero.name}}</h2>\n      <p>Heroes:</p>\n      <ul>\n        <li *ngFor=\"#hero of heroes\">\n          {{ hero.name }}\n          </li>\n      </ul>\n      <p *ngIf=\"heroes.length > 3\">There are many heroes!</p>\n    "
+                        templateUrl: 'app/app.component.html',
+                        directives: [
+                            click_me_component_1.ClickMeComponent, click_me2_component_1.ClickMeComponent2,
+                            loop_back_component_1.LoopbackComponent,
+                            keyup_components_1.KeyUpComponent_v1, keyup_components_1.KeyUpComponent_v2, keyup_components_1.KeyUpComponent_v3, keyup_components_1.KeyUpComponent_v4,
+                            little_tour_component_1.LittleTourComponent
+                        ]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- TourOfHeroes/angular2-tour-of-heroes/app/app.component.ts
+++ TourOfHeroes/angular2-tour-of-heroes/app/app.component.ts
@@ -1,26 +1,29 @@
 ﻿import {Component} from 'angular2/core';
+
+import {ClickMeComponent}    from './click-me.component';
+import {ClickMeComponent2}   from './click-me2.component';
+
+import {LoopbackComponent}   from './loop-back.component';
+
+import {KeyUpComponent_v1,
+KeyUpComponent_v2,
+KeyUpComponent_v3,
+KeyUpComponent_v4}   from './keyup.components';
+
+
+import {LittleTourComponent} from './little-tour.component';
+
+
+
 import {Hero} from './hero'
 @Component({
     selector: 'my-app',
-    template: `
-      <h1>{{title}}</h1>
-      <h2>My favorite hero is: {{myHero.name}}</h2>
-      <p>Heroes:</p>
-      <ul>
-        <li *ngFor="#hero of heroes">
-          {{ hero.name }}
-          </li>
-      </ul>
-      <p *ngIf="heroes.length > 3">There are many heroes!</p>
-    `
+    templateUrl: 'app/app.component.html',
+    directives: [
+        ClickMeComponent, ClickMeComponent2,
+        LoopbackComponent,
+        KeyUpComponent_v1, KeyUpComponent_v2, KeyUpComponent_v3, KeyUpComponent_v4,
+        LittleTourComponent
+    ]
 })
-export class AppComponent {
-    title = 'Tour of Heroes';
-    heroes = [
-        new Hero(1, 'Windstorm'),
-        new Hero(13, 'Bombasto'),
-        new Hero(15, 'Magneta'),
-        new Hero(20, 'Tornado')
-    ];
-    myHero = this.heroes[0];
-}
+export class AppComponent { }
