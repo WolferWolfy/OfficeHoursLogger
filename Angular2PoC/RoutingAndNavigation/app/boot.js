8914477a7ec883110a5e43c25a39a2e6db6c0953
Marton Imre Farkas <marton_imre_farkas@epam.com>
2016-01-29 10:16:36 +0100

    R&N - adding imports, app crash fix when using angular2-jwt

--- Angular2PoC/RoutingAndNavigation/app/boot.js
+++ Angular2PoC/RoutingAndNavigation/app/boot.js
@@ -1,18 +1,36 @@
-System.register(['angular2/platform/browser', 'angular2/router', './app.component'], function(exports_1) {
-    var browser_1, router_1, app_component_1;
+System.register(['angular2/platform/browser', './app.component', 'angular2/router', 'angular2/http', 'angular2-jwt', 'angular2/core'], function(exports_1) {
+    var browser_1, app_component_1, router_1, http_1, angular2_jwt_1, core_1;
     return {
         setters:[
             function (browser_1_1) {
                 browser_1 = browser_1_1;
             },
+            function (app_component_1_1) {
+                app_component_1 = app_component_1_1;
+            },
             function (router_1_1) {
                 router_1 = router_1_1;
             },
-            function (app_component_1_1) {
-                app_component_1 = app_component_1_1;
+            function (http_1_1) {
+                http_1 = http_1_1;
+            },
+            function (angular2_jwt_1_1) {
+                angular2_jwt_1 = angular2_jwt_1_1;
+            },
+            function (core_1_1) {
+                core_1 = core_1_1;
             }],
         execute: function() {
-            browser_1.bootstrap(app_component_1.AppComponent, [router_1.ROUTER_PROVIDERS]);
+            browser_1.bootstrap(app_component_1.AppComponent, [
+                http_1.HTTP_PROVIDERS,
+                router_1.ROUTER_PROVIDERS,
+                core_1.provide(angular2_jwt_1.AuthConfig, {
+                    useFactory: function () {
+                        return new angular2_jwt_1.AuthConfig();
+                    }
+                }),
+                angular2_jwt_1.AuthHttp // breaks with SyntaxError: expected expression, got '<' 	node_modules/angular2-jwt/angular2-jwt
+            ]);
         }
     }
 });
--- Angular2PoC/RoutingAndNavigation/app/boot.ts
+++ Angular2PoC/RoutingAndNavigation/app/boot.ts
@@ -1,4 +1,18 @@
 import {bootstrap}        from 'angular2/platform/browser';
-import {ROUTER_PROVIDERS} from 'angular2/router';
 import {AppComponent}     from './app.component';
-bootstrap(AppComponent, [ROUTER_PROVIDERS]);
+
+import {RouteConfig, Router, APP_BASE_HREF, ROUTER_PROVIDERS, ROUTER_DIRECTIVES, CanActivate} from 'angular2/router';
+import {HTTP_PROVIDERS, Http} from 'angular2/http';
+import {AuthHttp, AuthConfig, tokenNotExpired, JwtHelper} from 'angular2-jwt';
+import {provide} from 'angular2/core';
+
+bootstrap(AppComponent, [
+    HTTP_PROVIDERS,
+    ROUTER_PROVIDERS,
+    provide(AuthConfig, {
+        useFactory: () => {
+            return new AuthConfig();
+        }
+    }),
+    AuthHttp // breaks with SyntaxError: expected expression, got '<' 	node_modules/angular2-jwt/angular2-jwt
+]);
